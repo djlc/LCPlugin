7814dede48af62ac93df8acb92d4ee42f7f9983f
douken-right <douken-right>
2016-07-06 16:43:40 +0900

    cubeの仕様変更をしている途中

--- src/main/java/com/github/djlc/Cube.java
+++ src/main/java/com/github/djlc/Cube.java
@@ -24,34 +24,35 @@ public class Cube implements CommandExecutor, Listener {
 	private class Stats {
 		// 1辺の長さ
 		public int size = 0;
-		
+
 		// 立方体を構成するブロックの個数
 		public int num = 0;
-		
+
 		// ブロック
 		public Material material = null;
 		public MaterialData materialData = null;
 
 		// 座標
 		private Location location = null;
-		
+
+		@SuppressWarnings("unused")
 		public Stats(int size, Material material, MaterialData materialData) {
 			this.size			= size;
 			this.num			= size * size * size;
 			this.material		= material;
 			this.materialData	= materialData;
 		}
-		
+
 		public Stats(int size, Material material) {
 			this.size			= size;
 			this.num			= size * size * size;
 			this.material		= material;
 		}
 	}
-	
+
 	// 本体
 	private final LCPlugin plugin;
-	
+
 	// ユーザーを登録するリスト
 	private static HashMap<Player, Stats> list = new HashMap<Player, Stats>();
 
@@ -65,28 +66,35 @@ public class Cube implements CommandExecutor, Listener {
 		if (list.containsKey(event.getPlayer())) {
 			// ユーザーデータを取得して
 			Stats stats = list.get(event.getPlayer());
-			
+
 			// イベントから座標を取得して設定
 			stats.location = event.getBlock().getLocation().add(0, 1, 0);
-			
+
 			// 支払い処理して
 			if (!pay(event.getPlayer())) {
 				// 失敗したら中止してイベントキャンセル
+				event.setCancelled(true);
+				// リストから削除
+				list.remove(event.getPlayer());
 				return;
 			}
-			
+
 			// インベントリ内のブロックを差し引いて
 			removeItems(event.getPlayer());
-			
+
 			// 立方体生成
 			create(event.getPlayer());
+
+			// リストから削除
+			list.remove(event.getPlayer());
+			event.setCancelled(true);
 		}
 	}
 
 	private void create(Player p) {
-		
+
 		Stats stats = list.get(p);
-		
+
 		for (int i = 0; i < stats.size; i++) {
 			for (int j = 0; j < stats.size; j++) {
 				for (int k = 0; k < stats.size; k++) {
@@ -99,21 +107,15 @@ public class Cube implements CommandExecutor, Listener {
 			stats.location.setY(stats.location.getY() - stats.size);
 			stats.location.setZ(stats.location.getZ() + 1.0);
 		}
-		
+
 		// 使用したブロックの個数を通知
 		p.sendMessage(ChatColor.AQUA + "filled " + stats.num + " blocks.");
 	}
-	
+
 	private boolean pay(Player p) {
-		
+
 		Stats stats = list.get(p);
-		
-		// 料金が足りているか？
-		if (LCPlugin.econ.getBalance(p) < stats.num) {
-			p.sendMessage(ChatColor.RED + "You can't afford this!");
-			return false;
-		}
-		
+
 		// 支払い処理
 		EconomyResponse er = LCPlugin.econ.withdrawPlayer(p, stats.num);
 
@@ -122,13 +124,13 @@ public class Cube implements CommandExecutor, Listener {
 			p.sendMessage("An error occured: " + er.errorMessage);
 			return false;
 		}
-		
+
 		// 支払い通知
 		p.sendMessage("$" + er.amount + " was removed.");
-		
+
 		return true;
 	}
-	
+
 	private void removeItems(Player p) {
 		// プレイヤーのインベントリ
 		PlayerInventory pi = p.getInventory();
@@ -136,7 +138,7 @@ public class Cube implements CommandExecutor, Listener {
 		ItemStack is = null;
 		// ユーザーデータ
 		Stats stats = list.get(p);
-		
+
 		// 必要個数のブロックをインベントリから差し引く
 		int id = 0;
 		int num = stats.num;
@@ -165,31 +167,38 @@ public class Cube implements CommandExecutor, Listener {
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		
+
 		// 1辺nブロックの中の詰まった立方体を任意の固体ブロックで作成する.
 		if (cmd.getName().equalsIgnoreCase("cube")) {
-			
+
 			// プレイヤー
 			Player player = null;
-			
+
 			// Playerからのコマンドか？
 			if (sender instanceof Player) {
 				player = (Player) sender;
+				// すでに実行中なら中止する
+				if (list.containsKey(player)) {
+					sender.sendMessage(ChatColor.RED + "You have already run the command!");
+					return true;
+				}
 			} else {
 				plugin.getLogger().info(ChatColor.RED + "Please run this command in game.");
 				return true;
 			}
-			
+
 			// 引数の個数が違うなら
-			if (args.length != 2) {
+			if (args.length != 1) {
 				sender.sendMessage(ChatColor.RED + "Wrong Arguments!");
 				return false;
 			}
 
 			// 辺の長さ
 			int size = Integer.parseInt(args[0]);
+
 			// ブロック
-			Material material = Material.getMaterial(args[1]);
+			//Material material = player.get
+			//MaterialData materialData =
 
 			// nullなら名前が間違っていると通知
 			if (material == null) {
@@ -227,10 +236,16 @@ public class Cube implements CommandExecutor, Listener {
 				sender.sendMessage(ChatColor.RED + "You don't have enough Blocks!");
 				return true;
 			}
-			
+
+			// 料金が足りているか？
+			if (LCPlugin.econ.getBalance(player) < num) {
+				sender.sendMessage(ChatColor.RED + "You can't afford this!");
+				return true;
+			}
+
 			// プレイヤーを登録
 			list.put(player, new Stats(size, material));
-			
+
 			// プレイヤーが次にすべき操作を通知
 			sender.sendMessage(ChatColor.AQUA + "Please LEFT CLICK where you want to place the cube.");
 		}
