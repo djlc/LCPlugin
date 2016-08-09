ab4e0fd21e24d75d148466faf0e77cbaed5d0bf9
djlc <cdn60127@gmail.com>
2016-08-09 17:20:54 +0900

    [mod] reload, stopしてもTNTのデータを保持するようにした

--- src/main/java/com/github/djlc/LevelingTNT.java
+++ src/main/java/com/github/djlc/LevelingTNT.java
@@ -1,7 +1,9 @@
 package com.github.djlc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -11,6 +13,7 @@ import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
@@ -18,57 +21,84 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.ExplosionPrimeEvent;
+import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 
-public final class LevelingTNT implements Listener {
+public final class LevelingTNT implements Listener, MySerializable {
 
-	private static Map<Location, ItemMeta> blocks = new HashMap<Location, ItemMeta>();
-	private static List<ItemMeta> itemMetas = new ArrayList<ItemMeta>();
+	// keys
+	private static final String LEVELING_TNT = "levelingtnt";
 
-	public LevelingTNT() {
+	// Plugin
+	private final LCPlugin plugin;
+
+	// 配置された整地TNTの位置とItemMeta(種類)のハッシュ
+	// (シリアライズが必要)
+	private static Map<Location, ItemMeta> blocks = new HashMap<>();
+
+	// 独自TNTのItemMeta(種類)
+	private static Map<Integer, ItemMeta> itemMetas = new HashMap<>();
+
+	// TNTの爆破範囲のMap
+	private static Map<ItemMeta, Integer> radius = new HashMap<>();
+
+	public LevelingTNT(LCPlugin plugin) {
+
+		this.plugin = plugin;
+
 		// MK 1
-		addItemAndRecipe(new ItemStack(Material.TNT), "Leveling TNT MK1 (3x3)", "***", "*%*", "***",
+		addItemAndRecipe(new ItemStack(Material.TNT), 0, "Leveling TNT MK1 (3x3)", "***", "*%*", "***",
 				new HashMap<Character, Material>() {
 					{
 						put('*', Material.REDSTONE);
 						put('%', Material.TNT);
 					}
 				});
+		radius.put(itemMetas.get(0), 1);
+
 		// MK 2
-		addItemAndRecipe(new ItemStack(Material.TNT), "Leveling TNT MK2 (5x5)", "***", "*%*", "***",
+		addItemAndRecipe(new ItemStack(Material.TNT), 1, "Leveling TNT MK2 (5x5)", "***", "*%*", "***",
 				new HashMap<Character, Material>() {
 					{
 						put('*', Material.IRON_INGOT);
 						put('%', Material.TNT);
 					}
 				});
+		radius.put(itemMetas.get(1), 2);
+
 		// MK 3
-		addItemAndRecipe(new ItemStack(Material.TNT), ChatColor.AQUA + "Leveling TNT MK3 (9x9)", "***", "*%*", "***",
+		addItemAndRecipe(new ItemStack(Material.TNT), 2, ChatColor.AQUA + "Leveling TNT MK3 (9x9)", "***", "*%*", "***",
 				new HashMap<Character, Material>() {
 					{
 						put('*', Material.GOLD_INGOT);
 						put('%', Material.TNT);
 					}
 				});
+		radius.put(itemMetas.get(2), 4);
+
 		// MK 4
-		addItemAndRecipe(new ItemStack(Material.TNT), ChatColor.RED + "Leveling TNT MK4 (15x15)", "***", "*%*", "***",
-				new HashMap<Character, Material>() {
+		addItemAndRecipe(new ItemStack(Material.TNT), 3, ChatColor.RED + "Leveling TNT MK4 (15x15)", "***", "*%*",
+				"***", new HashMap<Character, Material>() {
 					{
 						put('*', Material.DIAMOND);
 						put('%', Material.TNT);
 					}
 				});
+		radius.put(itemMetas.get(3), 7);
+
+		// データ読み込み
+		deserialize();
 	}
 
-	private void addItemAndRecipe(ItemStack item, String name, String a, String b, String c,
+	private void addItemAndRecipe(ItemStack item, int itemID, String name, String a, String b, String c,
 			Map<Character, Material> list) {
 		// アイテムの追加
 		ItemMeta itemMeta = item.getItemMeta();
 		itemMeta.setDisplayName(name);
 		item.setItemMeta(itemMeta);
-		itemMetas.add(itemMeta);
+		itemMetas.put(itemID, itemMeta);
 
 		// レシピの追加
 		ShapedRecipe newItem = new ShapedRecipe(item);
@@ -79,6 +109,15 @@ public final class LevelingTNT implements Listener {
 
 		// レシピの登録
 		Bukkit.getServer().addRecipe(newItem);
+	}
+
+	private int getItemID(ItemMeta itemMeta) {
+		for (Map.Entry<Integer, ItemMeta> e : itemMetas.entrySet()) {
+			if (e.getValue().equals(itemMeta)) {
+				return e.getKey();
+			}
+		}
+		return -1;
 	}
 
 	// num*2+1の立方体の空洞を作る
@@ -93,7 +132,13 @@ public final class LevelingTNT implements Listener {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				for (int k = 0; k < n; k++) {
-					if (!location.getBlock().getType().equals(Material.BEDROCK)) location.getBlock().breakNaturally();
+					if (!location.getBlock().getType().equals(Material.BEDROCK)) {
+						if (location.getBlock().getType().equals(Material.TNT)) {
+							// ブロック登録解除
+							blocks.remove(location);
+						}
+						location.getBlock().breakNaturally();
+					}
 					location.setX(location.getX() + 1.0);
 				}
 				location.setX(location.getX() - n);
@@ -104,17 +149,12 @@ public final class LevelingTNT implements Listener {
 		}
 	}
 
+	// そのTNTが整地TNTかどうか
 	private boolean isMyItem(ItemMeta meta) {
-		boolean isMyItem = false;
-		for (ItemMeta im : itemMetas) {
-			if (im.equals(meta)) {
-				isMyItem = true;
-				break;
-			}
-		}
-		return isMyItem;
+		return itemMetas.containsValue(meta);
 	}
 
+	// ItemMetaからItemStackを取得する
 	private ItemStack getItemFromItemMeta(Material material, ItemMeta itemMeta) {
 		ItemStack item = new ItemStack(material);
 		item.setItemMeta(itemMeta);
@@ -122,7 +162,14 @@ public final class LevelingTNT implements Listener {
 	}
 
 	@EventHandler
-	void blockPlaced(BlockPlaceEvent event) {
+	public void onPluginDisable(PluginDisableEvent event) {
+		if (event.getPlugin().equals(plugin)) {
+			serialize();
+		}
+	}
+
+	@EventHandler
+	private void blockPlaced(BlockPlaceEvent event) {
 		// 置かれたブロックがTNTかどうか？
 		if (!event.getBlock().getType().equals(Material.TNT))
 			return;
@@ -144,7 +191,7 @@ public final class LevelingTNT implements Listener {
 	}
 
 	@EventHandler
-	void blockBroke(BlockBreakEvent event) {
+	private void blockBroke(BlockBreakEvent event) {
 		// ブロックの位置を取得
 		Location location = event.getBlock().getLocation();
 		if (!blocks.containsKey(location) || !event.getBlock().getType().equals(Material.TNT)) {
@@ -161,12 +208,13 @@ public final class LevelingTNT implements Listener {
 			event.getPlayer().getWorld().dropItemNaturally(location,
 					getItemFromItemMeta(Material.TNT, blocks.get(location)));
 		}
+
 		// ブロック登録解除
 		blocks.remove(location);
 	}
 
 	@EventHandler
-	void blockExplode(ExplosionPrimeEvent event) {
+	private void blockExplode(ExplosionPrimeEvent event) {
 		// Entityを取得
 		Entity entity = event.getEntity();
 
@@ -194,20 +242,64 @@ public final class LevelingTNT implements Listener {
 		event.setCancelled(true);
 
 		// 立方体型に爆破
-		int size = 0;
-		if (itemMetas.get(0).equals(blocks.get(location))) {
-			size = 1;
-		} else if (itemMetas.get(1).equals(blocks.get(location))) {
-			size = 2;
-		} else if (itemMetas.get(2).equals(blocks.get(location))) {
-			size = 4;
-		} else if (itemMetas.get(3).equals(blocks.get(location))) {
-			size = 7;
-		}
 		tnt.getWorld().playSound(location, Sound.ENTITY_GENERIC_EXPLODE, 1, 1);
-		explosion(location, size);
+		explosion(location.clone(), radius.get(blocks.get(location)));
 
 		// 登録解除
 		blocks.remove(location);
+		refleshList();
+	}
+
+	private void refleshList() {
+		for (Iterator<Location> i = blocks.keySet().iterator(); i.hasNext();) {
+			if (!i.next().getBlock().getType().equals(Material.TNT)) {
+				i.remove();
+			}
+		}
+	}
+
+	@Override
+	public void serialize() {
+		// world名, <itemID, 座標>
+		Map<String, Map<Integer, List<List<Double>>>> data = new HashMap<>();
+		for (Map.Entry<Location, ItemMeta> e : blocks.entrySet()) {
+			if (!data.containsKey(e.getKey().getWorld().getName())) {
+				data.put(e.getKey().getWorld().getName(), new HashMap<Integer, List<List<Double>>>());
+			}
+			if (!data.get(e.getKey().getWorld().getName()).containsKey(getItemID(e.getValue()))) {
+				data.get(e.getKey().getWorld().getName()).put(getItemID(e.getValue()), new ArrayList<List<Double>>());
+			}
+			data.get(e.getKey().getWorld().getName()).get(getItemID(e.getValue()))
+					.add(Arrays.asList(e.getKey().getX(), e.getKey().getY(), e.getKey().getZ()));
+		}
+		List<Map<String, Map<Integer, List<List<Double>>>>> temp = new ArrayList<>();
+		temp.add(data);
+		plugin.getConfig().set(LEVELING_TNT, temp);
+	}
+
+	@Override
+	public void deserialize() {
+		FileConfiguration conf = plugin.getConfig();
+		if (conf.contains(LEVELING_TNT)) {
+			List<Map<?, ?>> list = conf.getMapList(LEVELING_TNT);
+			if (list.size() == 0)
+				return;
+			Map<?, ?> map = list.get(0);
+			for (Map.Entry<?, ?> e : map.entrySet()) {
+				String worldName = (String) e.getKey();
+				@SuppressWarnings("unchecked")
+				Map<Integer, List<List<Double>>> hash = (Map<Integer, List<List<Double>>>) e.getValue();
+				for (Map.Entry<Integer, List<List<Double>>> f : hash.entrySet()) {
+					for (int i = 0; i < f.getValue().size(); i++) {
+						List<Double> g = f.getValue().get(i);
+						Location l = new Location(Bukkit.getWorld(worldName), g.get(0), g.get(1), g.get(2));
+						if (l.getBlock().getType().equals(Material.TNT)) {
+							blocks.put(l, itemMetas.get(f.getKey()));
+							System.out.println(l.toString() + " was loaded.");
+						}
+					}
+				}
+			}
+		}
 	}
 }
